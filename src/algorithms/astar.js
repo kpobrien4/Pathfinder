556ac8d14d347bcb2e625e9b9db8e8e99cf6ec97
Kevin O'Brien <kpobrien@buffalo.edu>
2019-12-21 14:56:42 -0500

    added dijkstra pathfinding

---
+++ src/algorithms/astar.js
@@ -0,0 +1,11 @@
+export function astar(grid, startNode, finishNode) {
+    const openSet = []
+    const closedSet = []
+    startNode.distance = 0;
+    openSet.push(startNode)
+
+
+    while (
+        openSet.length > 0
+    )
+}
